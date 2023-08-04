8451530455f8eea7ff8809d47cec7070d1d2f4be
DenysPetroff <petroffich2008@gmail.com>
2023-08-04 23:47:56 +0300

    20230804 -23_44
    зробив синім останнє слово в заголовку,
    розмітка промальовується

--- src/js/books-from-category.js
+++ src/js/books-from-category.js
@@ -1,5 +1,8 @@
+import { title } from 'process';
 import { getOneCategoriesBooksFromApi } from './fetch_API_categories.js';
-const booksSection = document.querySelector('.container');
+import { Report } from 'notiflix/build/notiflix-report-aio';
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
+const booksSection = document.querySelector('.books-list');
 // const button = document.querySelector('.see-more-btn');
 // button.addEventListener('click', handlerSeeMoreBtn);
 // console.dir(button);
@@ -12,30 +15,24 @@ export function handlerSeeMoreBtn(categories) {
   category = categories;
   getOneCategoriesBooksFromApi(categories).then(resp => {
     console.log('resp', resp);
-    if (resp.length = 0) { Report.failure(
-      'Sorry!',
-      'There are no books in this category'
-    );
-
-    }
-    // Loading.remove();
-    createGalleryCategoriesBooksMarkup(resp);
-    cardsListners();
-
+          // Loading.remove();
+      createGalleryCategoriesBooksMarkup(resp);
+      cardsListners();
+    
     // blueWord(categories);
   }).catch(error => {
     console.log(error);
-    // Report.failure(
-    //   'Oops!',
-    //   'Something went wrong! Try reloading the page!',
-    //   'Okay',
-    // );
+    Report.failure(
+      'Oops!',
+      'Something went wrong! Try reloading the page!',
+      'Okay',
+    );
   })
 }
 
 
 function createGalleryCategoriesBooksMarkup(data) { 
-  const categoryHeader = `<h2 class="books-section-title category-books-title"> <span class="last-word"></span></h2>`;
+  const categoryHeader = `<h2 class="books-section-title category-books-title"> </h2>`;
      
    const cartBook = data.map(({_id, book_image, title, author }) => 
 // //    `<div class="photo-card">
@@ -57,22 +54,17 @@ function createGalleryCategoriesBooksMarkup(data) {
   booksSection.insertAdjacentHTML('afterbegin', categoryHeader);
   const booksCategContainer = document.querySelector('.books-container');
    booksCategContainer.insertAdjacentHTML('beforeend', cartBook);
-  // blueWord(categories);
-    const categoryBooksHeader = document.querySelector('.books-section-title');
-  const lastWordSpan = categoryBooksHeader.querySelector('.last-word');
+  
+  const categoryBooksHeader = document.querySelector('.books-section-title');
   const categoriesWordsArr = category.split(' ');
   const lastCategoryWord = categoriesWordsArr[categoriesWordsArr.length - 1];
   const allWordsExceptLast = categoriesWordsArr.slice(0, -1).join(' ');
   categoryBooksHeader.textContent = allWordsExceptLast;
   console.log(lastCategoryWord);
-  lastWordSpan.textContent = lastCategoryWord;
-  lastWordSpan.classList.add('blue-word');
-  console.log(lastWordSpan);
+   const titleCategory = document.querySelector('.category-books-title');
+  titleCategory.insertAdjacentHTML('beforeend', ` <span class="last-word">${lastCategoryWord}</span>`);
+ 
 };
-
-// function blueWord(categories) {
-
-// }
 
 function cardsListners() {
   const bookCards = document.querySelectorAll('.book-card');
