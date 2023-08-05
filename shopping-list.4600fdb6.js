9ccc8aa905793a264e25b328fc785fe5cd32b202
bakkara <50306713+bakkara@users.noreply.github.com>
2023-08-05 19:50:10 +0000

    Deploying to gh-pages from @ bakkara/project02-dreamteam@73d314e10918ec7dbd91ce9d7206bd6cc78f156f 🚀

---
+++ shopping-list.4600fdb6.js
@@ -0,0 +1,15 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},o={},t={},i=e.parcelRequired7c6;null==i&&((i=function(e){if(e in o)return o[e].exports;if(e in t){var i=t[e];delete t[e];var s={id:e,exports:{}};return o[e]=s,i.call(s.exports,s,s.exports),s.exports}var l=Error("Cannot find module '"+e+"'");throw l.code="MODULE_NOT_FOUND",l}).register=function(e,o){t[e]=o},e.parcelRequired7c6=i);var s=i("2shzp");const l=document.querySelector("#bookList"),n=document.querySelector("#message");async function a(e){try{let o=await (0,s.default).get(`https://books-backend.p.goit.global/books/bookId/${e}`);return o.data}catch(e){console.error("Error fetching book data:",e)}}!function(){let e=JSON.parse(localStorage.getItem("shoppingListBooks"))||[];0===e.length?n.style.display="block":e.forEach(async e=>{let o=await a(e.id);o&&function(e){let o=document.createElement("div");o.innerHTML=`
+      <div class="book-card">
+        <img class="book-image" src="${e.image}" alt="Book Image">
+        <div class="book-title">${e.title}</div>
+        <div class="book-category">${e.category}</div>
+        <div class="book-description">${e.description}</div>
+        <div class="book-author">Author: ${e.author}</div>
+        <div class="book-links">
+          ${e.links.map(e=>`<div class="book-link"><a href="${e}">Buy on ${e}</a></div>`).join("")}
+        </div>
+        <button class="delete-button"><svg width="16" height="16" class="shopping-svg">
+        <use href="./images/symboldefs.svg#btn-shopping-list"></use></button>
+      </div>
+    `,o.querySelector(".delete-button").addEventListener("click",()=>{(function(e){let o=booksFromLocalStorage.filter(o=>o.title!==e.title);localStorage.setItem("shoppingListBooks",JSON.stringify(o)),0===o.length&&(n.style.display="block")})(e),o.remove()}),l.appendChild(o)}(o)})}();
+//# sourceMappingURL=shopping-list.4600fdb6.js.map
