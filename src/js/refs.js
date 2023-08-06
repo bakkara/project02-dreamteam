783accfa922ed9a75a2ea9cff6de9170d507d6be
bakkara <ane4karymar@gmail.com>
2023-08-06 22:11:03 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -10,17 +10,6 @@ const elements = {
 console.log(elements);
 export { elements };
 
-export const blocks = [
-  {
-    id: '01',
-    title: 'Save the Children',
-    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: './images/image1.png',
-  },
-  {
-    id: '02',
-=======
-
 import img1 from '../images/image1.png'
 import img2 from '../images/image2.png'
 import img3 from '../images/image3.png'
@@ -31,16 +20,7 @@ import img7 from '../images/image7.png'
 import img8 from  '../images/image8.png'
 import img9 from  '../images/image9.png'
 
- const elements = {
-    BooksInfo: document.querySelector('.books-list'),
-    
-}
-
-export { elements }
-
-
-
-export const blocks =  [
+ export const blocks =  [
     {
     id: 1,
     title: 'Save the Children',
@@ -48,62 +28,17 @@ export const blocks =  [
     img: `${img1}`,
   },
     {
-    id: 2,
-
+     id: 2,
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
     img: `${img2}`,
   },
-
-  {
-    id: '03',
-
     {
     id: 3,
-
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
     img: `${img3}`,
   },
-
-  {
-    id: '04',
-    title: 'International Medical Corps',
-    url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: './src/images/image4.png',
-  },
-  //     {
-  //     id: '05',
-  //     title: 'Medicins Sans Frontieres',
-  //     url: 'https://www.msf.org/ukraine',
-  //     img: './src/images/image5.png',
-  //   },
-  //     {
-  //     id: '06',
-  //     title: 'RAZOM',
-  //     url: 'https://www.razomforukraine.org/',
-  //     img: './src/images/image6.png',
-  //   },
-  //     {
-  //     id: '07',
-  //     title: 'Action against hunger',
-  //     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-  //     img: './src/images/image7.png',
-  //   },
-  //     {
-  //     id: '08',
-  //     title: 'World vision',
-  //     url: 'https://www.wvi.org/emergencies/ukraine',
-  //     img: './src/images/image8.png',
-  //   },
-  //     {
-  //     id: '09',
-  //     title: 'Serhiy Prytula Charity Foundation',
-  //     url: 'https://prytulafoundation.org/en',
-  //     img: './src/images/image9.png',
-  //   },
-];
-
     {
     id: 4,  
     title: 'International Medical Corps',
@@ -141,4 +76,3 @@ export const blocks =  [
     img: `${img9}`,
   },
 ]
-
