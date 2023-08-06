e0c24fd09cda1d6ee6a7f330a567e5c199b008b9
bakkara <ane4karymar@gmail.com>
2023-08-06 14:53:48 +0300

    update signin

--- src/js/fire-base.js
+++ src/js/fire-base.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth"
+import { getFirestore, setDoc, doc } from "firebase/firestore"; 
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBZmcnC_2fBns-z8P5Wk9Fw_wxB6fZENSM",
@@ -11,43 +13,64 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const submit = document.querySelector('#submitData')
+const signUpBtn = document.querySelector('#signup')
+const signInBtn = document.querySelector('#signin')
 const auth = getAuth();
+const db = getFirestore(app)
 
 
-submit.addEventListener('click', signUp)
+signUpBtn.addEventListener('click', signUp)
+signInBtn.addEventListener('click', signIn)
 
-function signUp(evt) {
+async function signUp(evt) {
+  
     try {
-        const email = document.querySelector('#email').value;
-    const password = document.querySelector('#psw').value;
-        const userName = document.querySelector('#userName').value;
-    } catch (error) {
+      const email = document.querySelector('#email').value;
+      const password = document.querySelector('#psw').value;
+      const userName = document.querySelector('#userName').value;
+      const userAuth = await createUserWithEmailAndPassword(auth, email, password);
+      const uid = userAuth.user.uid
+      const userObj = {
+        userName,
+        email,  
+        accountActivate: true,
+        uid
+      }
+      console.log("authObj", userObj)
+      const userRef = doc(db, "users", userAuth.uid,);
+      const userDB = await setDoc(userRef, userObj);
+      console.log("authObj", userDB);
+      window.location.replace('/')
+      } catch (error) {
         const errorMessage = error.message;
         alert(errorMessage)
     }
-    createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-      const user = userCredential.user;
-      alert('user created')
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage)
-    // ..
-  });
+ 
 }
 
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+async function signIn(evt) {
+  
+    try {
+      const email = document.querySelector('#email').value;
+      const password = document.querySelector('#psw').value;
+      const userName = document.querySelector('#userName').value;
+      const userLogin = await signInWithEmailAndPassword(auth, email, password);
+      localStorage.setItem("userUid", userLogin.user.uid)
+      localStorage.setItem("userName", userName)
+      window.location.replace('/index')
+      } catch (error) {
+        const errorMessage = error.message;
+        alert(errorMessage)
+    }
+ 
+}
+// signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//   });
