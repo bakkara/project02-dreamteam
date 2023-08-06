da7f48ed51741691e96c1945635a82ed80c13c70
Volodymyr <buvoro@gmail.com>
2023-08-06 20:45:25 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,63 +1,67 @@
 const elements = {
-    BooksInfo: document.querySelector('.books-list'),
+  BooksInfo: document.querySelector('.books-list'),
+  modalMainBtn: document.querySelector('.js-modal-btn'),
+  modalStores: document.querySelectorAll('.js-modal-link'),
+  modalBookCard: document.querySelector('.js-modal-book'),
+  modalBtnClose: document.querySelector('.js-modal-close'),
+  modal: document.querySelector('.js-modal'),
+};
+console.log(elements);
+export { elements };
 
-}
-
-export { elements }
-
-export const blocks =  [
-    {
+export const blocks = [
+  {
     id: '01',
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: "./images/image1.png",
+    img: './images/image1.png',
   },
-    {
+  {
     id: '02',
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
     img: '/src/images/image2.png',
   },
-    {
+  {
     id: '03',
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
     img: './src/images/image3.png',
   },
-    {
-    id: '04',  
+  {
+    id: '04',
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
     img: './src/images/image4.png',
   },
-//     {
-//     id: '05',  
-//     title: 'Medicins Sans Frontieres',
-//     url: 'https://www.msf.org/ukraine',
-//     img: './src/images/image5.png',
-//   },
-//     {
-//     id: '06',  
-//     title: 'RAZOM',
-//     url: 'https://www.razomforukraine.org/',
-//     img: './src/images/image6.png',
-//   },
-//     {
-//     id: '07',  
-//     title: 'Action against hunger',
-//     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-//     img: './src/images/image7.png',
-//   },
-//     {
-//     id: '08',  
-//     title: 'World vision',
-//     url: 'https://www.wvi.org/emergencies/ukraine',
-//     img: './src/images/image8.png',
-//   },
-//     {
-//     id: '09',  
-//     title: 'Serhiy Prytula Charity Foundation',
-//     url: 'https://prytulafoundation.org/en',
-//     img: './src/images/image9.png',
-//   },
-]
+  //     {
+  //     id: '05',
+  //     title: 'Medicins Sans Frontieres',
+  //     url: 'https://www.msf.org/ukraine',
+  //     img: './src/images/image5.png',
+  //   },
+  //     {
+  //     id: '06',
+  //     title: 'RAZOM',
+  //     url: 'https://www.razomforukraine.org/',
+  //     img: './src/images/image6.png',
+  //   },
+  //     {
+  //     id: '07',
+  //     title: 'Action against hunger',
+  //     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+  //     img: './src/images/image7.png',
+  //   },
+  //     {
+  //     id: '08',
+  //     title: 'World vision',
+  //     url: 'https://www.wvi.org/emergencies/ukraine',
+  //     img: './src/images/image8.png',
+  //   },
+  //     {
+  //     id: '09',
+  //     title: 'Serhiy Prytula Charity Foundation',
+  //     url: 'https://prytulafoundation.org/en',
+  //     img: './src/images/image9.png',
+  //   },
+];
