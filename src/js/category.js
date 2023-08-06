a781ec0297c34ac0f9e19920a81e91e8502e5d8c
ANastya28 <guselnikova2803@gmail.com>
2023-08-06 03:39:11 +0300

    Created List Category

--- src/js/category.js
+++ src/js/category.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+// import 'overlayscrollbars/overlayscrollbars.css';
+import { OverlayScrollbars } from 'overlayscrollbars';
 
 const URL = 'https://books-backend.p.goit.global/books/category-list';
 const categoryList = document.querySelector('.category-list');
@@ -12,7 +14,13 @@ function renderCategoryList(data) {
   return data
     .map(
       ({ list_name }) =>
-        `<li class="categories-list-item">${list_name}</li>`
+        `
+            <li class="category-li">
+                <a class="category" href="#">
+                    <span class="category-span">${list_name}</span>
+                </a>
+            </li>
+        `
     )
     .join('');
 }
@@ -26,13 +34,61 @@ getCategoryList()
   )
   .catch(error => console.log(error));
 
+// const osInstance = OverlayScrollbars(document.querySelector('#myElement'), {});
 
-function onCategoryClick(event) {
-    console.log(event)
+OverlayScrollbars({ 
+    target: document.querySelector('#myElement') 
+}, {
+    showNativeOverlaidScrollbars: true
+});
+
+
+categoryList.addEventListener('click', onclickCategory);
+
+function onclickCategory(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'SPAN') {
+        return
+    }
     if (event.target.textContent === 'All categories') {
-       event.target.classList.remove('active')
+        removeUpperCase();
+        makeUpperCase(event.target);
     } else {        
-        event.target.classList.add('active')
+        removeUpperCase();
+        makeUpperCase(event.target);
     }
 }
 
+function makeUpperCase(data) {
+    data.classList.add('upper-case');
+};
+
+function removeUpperCase() {
+    const rem = document.querySelectorAll('.category-span');
+    rem.forEach(el => el.classList.remove('upper-case'))
+};
+
+function seeMoreFunc(data) {
+    removeUpperCase();
+    const newDom = document.querySelectorAll('.category-span');
+    newDom.forEach(el => {
+        if (el.textContent === data) el.classList.add('upper-case')
+    });
+}
+
+
+categoryList.addEventListener('mouseover', MouseClickAccentOn);
+categoryList.addEventListener('mouseout', MouseClickAccentOff);
+
+function MouseClickAccentOn(event) {
+    if (event.target.nodeName !== 'SPAN') {
+        return;
+    }
+    const line = event.target;
+    line.classList.add('active');
+};
+
+function MouseClickAccentOff(event) {
+    const line = event.target;
+    line.classList.remove('active');
+};
