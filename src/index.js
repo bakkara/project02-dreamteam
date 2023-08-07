911303c05decee0aef6b5c2bfd54615f870c41e0
bakkara <ane4karymar@gmail.com>
2023-08-07 16:02:05 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-
-import './js/top-books.js'
 import './js/header.js'
-
-import './js/fire-base.js'
-
 import './js/burger.js'
-
-import './js/top-books.js';
+import './js/fire-base.js'
+import './js/api.js'
+import './js/fetch_API_categories';
+import './js/category.js';
+import './js/top-books.js'
 import './js/books-from-category.js';
 import './js/modal-window.js';
-import './js/top-books.js';
-import './js/books-from-category.js';
-import './js/fire-base';
 import './js/support.js';
-import './js/category.js';
-
+import './js/storage.js';
 import './js/scroll-up.js';
-
+import './js/refs.js';
+import './js/markup.js';
 import './js/footer.js'
 
