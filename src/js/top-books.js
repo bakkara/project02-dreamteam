9f0533d5864fa83ad0b9ab74cb0edc50cbc8c388
vika-tsymbaliyk <vika.bronytska@gmail.com>
2023-08-10 19:38:21 +0300

    hendler-pagination

--- src/js/top-books.js
+++ src/js/top-books.js
@@ -6,14 +6,32 @@ import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { modal } from './modal-window.js';
 import { createCartBookMarcup } from './markup.js';
 
-let countTopCategories = 4;
+Loading.standard('Loading data, please wait...', {
+  backgroundColor: 'rgba(0.5,0,0,0.2)',
+});
 
-function displayCategories(data, countTopCategories) {
+let countTopCategories = 4;
+let startCount = 0;
+
+let scrollToTopBtnEl = document.querySelector('.scroll-up-btn');
+const bookGuard = document.querySelector('.books-guard')
+
+const options = {
+    root: null,
+    rootMargin: "600px",
+    threshold: 0,
+};
+
+const observer = new IntersectionObserver(handlerPagination, options);
+
+
+function displayCategories(data) {
   // let dataToShow = data.slice(0,)
-  elements.BooksInfo.innerHTML = "";
-  const categoriesHtml = data.slice(0,countTopCategories).map(createTopBooks).join('');
-  elements.BooksInfo.innerHTML = ` <h2 class="books-section-title">Best Sellers <span class="last-word">Books</span> </h2>`
+  // elements.BooksInfo.innerHTML = "";
+  const categoriesHtml = data.slice(startCount,countTopCategories).map(createTopBooks).join('');
+  // elements.BooksInfo.innerHTML = ` <h2 class="books-section-title">Best Sellers <span class="last-word">Books</span> </h2>`
   elements.BooksInfo.insertAdjacentHTML("beforeend",categoriesHtml) ;
+  const guard = observer.observe(bookGuard)
   return ""
 }
 
@@ -38,8 +56,10 @@ function createTopBooks(data) {
     </div>`;
 }
 
+
 window.addEventListener('resize', renderTopBooks);
 renderTopBooks();
+
 
 function addEventListenersToButtons() {
   const seeMoreButtons = document.querySelectorAll('.see-more-btn');
@@ -49,13 +69,17 @@ function addEventListenersToButtons() {
   });
 }
 
-function renderTopBooks() {
+export async function renderTopBooks() {
+
+  elements.BooksInfo.innerHTML = "";
+  elements.BooksInfo.innerHTML = ` <h2 class="books-section-title">Best Sellers <span class="last-word">Books</span> </h2>`
+  
   fetchTopBooks()
     .then(data => {
-        Loading.remove();
-        // console.log(data);
-        const markup = displayCategories(data, countTopCategories);
+        
+        const markup = displayCategories(data);
         elements.BooksInfo.insertAdjacentHTML("beforeend", markup);
+        Loading.remove();
         addEventListenersToBooks();
         addEventListenersToButtons();
     })
@@ -88,4 +112,43 @@ function onOpenBook(bookId) {
 }
 
 
-export {renderTopBooks,displayCategories, countTopCategories}
+function handlerPagination(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      
+        countTopCategories += 4;
+        startCount += 4;
+        Loading.standard('Loading data, please wait...', {
+          backgroundColor: 'rgba(0.5,0,0,0.2)',
+        });
+
+        fetchTopBooks()
+        .then(data => {
+            
+            const markup = displayCategories(data);
+            elements.BooksInfo.insertAdjacentHTML("beforeend", markup);
+            Loading.remove();
+            addEventListenersToBooks();
+            addEventListenersToButtons();
+            if (data.lenght <= countTopCategories){
+              observer.unobserve(entries.target)
+            }
+            
+        })
+        .catch((error) => {
+            Report.failure(
+                'Oops!',
+                'Something went wrong! Try reloading the page!',
+                'Okay',
+            );
+            console.log(error);
+        }
+        )
+            .finally(
+                Loading.remove()
+            );
+    }
+    })
+}
+
+// export {displayCategories, countTopCategories}
